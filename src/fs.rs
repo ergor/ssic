9af6897c8cc6f6e1729a98096e703052ebe50367
ergor <erol@gorancic.no>
2021-02-20 16:03:24 +0100

    Use walkdir for file scanning. Modified CLI.

--- src/fs.rs
+++ src/fs.rs
@@ -8,34 +8,15 @@ pub struct Scan {
     pub other_files: Vec<DirEntry>
 }
 
-pub fn scan(path: PathBuf) -> io::Result<Scan> {
+pub fn scan(path: PathBuf, follow_links: bool) -> io::Result<Scan> {
 
     let mut scan_result = Scan {
         shtml_files: Vec::new(),
         other_files: Vec::new()
     };
 
-    let mut dir_iter_stack: Vec<RefCell<ReadDir>> = Vec::new();
+    for entry in walkdir::WalkDir::new(path).follow_links(follow_links) {
 
-    let root_dir_iter = std::fs::read_dir(path)?;
-    dir_iter_stack.push(RefCell::new(root_dir_iter));
-
-    while !dir_iter_stack.is_empty() {
-        // safety: we know it's Some, because of loop condition
-        let current_dir_iter = dir_iter_stack.last().unwrap();
-
-        let next_child_opt = current_dir_iter.borrow_mut().next();
-        if let Some(child) = next_child_opt {
-            match child {
-                Ok(child) => {
-                    // if ends with shtml or has x bit set, push to shtml files
-                    // else push to other files
-
-                    if child.file_type()
-                },
-                Err(e) => {}
-            }
-        }
     }
 
     return Ok(scan_result);
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,13 @@ fn main() -> io::Result<()> {
     let app = clap::App::new(clap::crate_name!())
         .version(clap::crate_version!())
         .about("Server Side Includes Compiler")
-        .arg(clap::Arg::with_name("SRCDIR")
-            .help("Source directory")
-            .required(true)
-            .index(1))
+        .arg(clap::Arg::with_name("indir")
+            .short("i")
+            .long("in")
+            .value_name("DIRECTORY")
+            .help("Source directory. Defaults to $PWD")
+            .required(false)
+            .takes_value(true))
         .arg(clap::Arg::with_name("outdir")
             .short("o")
             .long("out")
@@ -18,21 +21,38 @@ fn main() -> io::Result<()> {
             .help("Output directory. Defaults to $PWD/dist")
             .required(false)
             .takes_value(true))
+        .arg(clap::Arg::with_name("follow-links")
+            .short("l")
+            .long("follow-links")
+            .help("If given, will follow symlinks")
+            .required(false)
+            .takes_value(false))
     ;
     let matches = app.get_matches();
 
-    let mut default_out = std::env::current_dir()?;
-    default_out.push("dist");
+    let pwd = std::env::current_dir()?;
 
-    let outdir = matches.value_of("outdir")
-        .map_or(default_out, |path| PathBuf::from(path));
+    let indir: PathBuf = {
+        let default_in = pwd.clone();
 
+        matches.value_of("indir")
+            .map_or(default_in, |indir_val| PathBuf::from(indir_val))
+    };
+
+    let outdir: PathBuf = {
+        let mut default_out = pwd.clone();
+        default_out.push("dist");
+
+        matches.value_of("outdir")
+            .map_or(default_out, |outdir_val| PathBuf::from(outdir_val))
+    };
+
+    let follow_links = matches.is_present("follow-links");
+
+    println!("Using input directory: '{}'", indir.display());
     println!("Using output directory: '{}'", outdir.display());
 
-    // safety: this argument is required by clap and thus is always Some<T>
-    let srcdir = PathBuf::from(matches.value_of("SRCDIR").unwrap());
-
-    fs::scan(srcdir);
+    let indir_scan = fs::scan(indir, follow_links);
 
     return Ok(());
 }
